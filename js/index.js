cd183166dec34b4c39f06804b33b0460c208f0f0
miriamAtFlatiron <miriam.peskowitz@flatironschool.com>
2021-06-09 17:41:18 -0400

    second option, fetch works'

--- js/index.js
+++ js/index.js
@@ -7,37 +7,40 @@
 const url = "http://localhost:3000/monsters"
 const monsters = []
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
     getFiftyMonsters()
 })
 
+
 const getFiftyMonsters = () => {
   fetch("http://localhost:3000/monsters/?_limit=50")
   .then(res => res.json())
-  .then(data => data.forEach(putMonstersOnPage(monster))
-  .then(console.log(data))
+  .then(monster => monster.forEach(putMonstersOnPage)
   )
 }
 
-const putMonstersOnPage = (monster) => {
-  let monsterContainer = document.querySelector('#monster-container')
-    let name = document.createElement('h2')
-    let age = document.createElement('h4')
-    let description = document.createElement('hp')
-    name.innerText = monster.name
-    age.innerText = monster.age
-    description.innerText = monster.description
-    monsterContainer.append(name,age,description)
-}
+// const putMonstersOnPage = (monster) => {
+//   let monsterContainer = document.querySelector('#monster-container')
+//     let name = document.createElement('h2')
+//     let age = document.createElement('h4')
+//     let description = document.createElement('hp')
+//     name.innerText = monster.name
+//     age.innerText = monster.age
+//     description.innerText = monster.description
+//     monsterContainer.append(name,age,description)
+// }
 
-also: 
-const monsterContainerDiv = document.querySelector("#monster-container")
-const renderMonster = (monster) => {
+// also: THIS IS WAY MORE ELEGANT
+
+const putMonstersOnPage = (monster) => {
+  const monsterContainer = document.querySelector("#monster-container")
   let monsterDiv = document.createElement('div')
     monsterDiv.innerHTML = `
      <h2>${monster.name}</h2>
      <h4>${monster.age}</h4>
      <p>${monster.description}</p>
     `
-    monsterContainer.append(monsterDiv)
+  monsterContainer.append(monsterDiv)
 }
